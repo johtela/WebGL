c8f5cf2dccd0f7deb22b708402946755a686a4d6
Johtela Tommi <tommi.johtela@lindorff.com>
2017-09-15 18:34:57 +0300

    Added mocha and tried to get test to run.

--- Tests.ts
+++ Tests.ts
@@ -1,4 +1,11 @@
-///<reference path="./node_modules/jsverify/lib/jsverify.js"/>
+///<reference path="./node_modules/jsverify/lib/jsverify.d.ts"/>
 
 import * as jsc from "jsverify";
 
+describe ("foobar", () =>
+{
+    // forall (f : bool -> bool) (b : bool), f (f (f b)) = f(b).
+    jsc.property ("foobar2", "bool -> bool", "bool", (f, b) =>
+        f(f(f(b))) === b
+    );
+});
--- js/Tests.js
+++ js/Tests.js
@@ -1,3 +1,11 @@
 "use strict";
+///<reference path="./node_modules/jsverify/lib/jsverify.d.ts"/>
 Object.defineProperty(exports, "__esModule", { value: true });
+var jsc = require("jsverify");
+describe("foobar", function () {
+    // forall (f : bool -> bool) (b : bool), f (f (f b)) = f(b).
+    jsc.property("foobar2", "bool -> bool", "bool", function (f, b) {
+        return f(f(f(b))) === b;
+    });
+});
 //# sourceMappingURL=Tests.js.map
