405a5d448550c8f67f2a3c6a379d62f47f2b0e11
Johtela Tommi <tommi.johtela@lindorff.com>
2017-10-11 17:37:03 +0300

    Intermediate commit.

--- js/src/VertexAttribute.js
+++ js/src/VertexAttribute.js
@@ -1,7 +1,23 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
+var Vertex = (function () {
+    function Vertex() {
+    }
+    return Vertex;
+}());
+exports.Vertex = Vertex;
 var VertAttr = (function () {
-    function VertAttr(type, count, getter) {
+    function VertAttr(name, type, count, getter) {
         this.type = type;
         this.count = count;
         this.getter = getter;
@@ -26,42 +42,52 @@ var VertAttr = (function () {
     return VertAttr;
 }());
 exports.VertAttr = VertAttr;
-function lift(fun) {
-    return function (obj) { return [fun(obj)]; };
-}
-function byte(gl, getter) {
-    return new VertAttr(gl.BYTE, 1, lift(getter));
+var Foo = (function (_super) {
+    __extends(Foo, _super);
+    function Foo() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Foo;
+}(Vertex));
+function byte(gl, vertex, name) {
+    var val = vertex[name];
+    return new VertAttr(name, gl.BYTE, 1, function (v) { return [v[name]]; });
 }
 exports.byte = byte;
-function short(gl, getter) {
-    return new VertAttr(gl.SHORT, 1, lift(getter));
+function test(gl) {
+    var foo = new Foo();
+    var a = byte(gl, foo, "bar");
 }
-exports.short = short;
-function ubyte(gl, getter) {
-    return new VertAttr(gl.UNSIGNED_BYTE, 1, lift(getter));
-}
-exports.ubyte = ubyte;
-function ushort(gl, getter) {
-    return new VertAttr(gl.UNSIGNED_SHORT, 1, lift(getter));
-}
-exports.ushort = ushort;
-function float(gl, getter) {
-    return new VertAttr(gl.FLOAT, 1, lift(getter));
-}
-exports.float = float;
-function liftVec(getter) {
-    return function (obj) { return getter(obj).toArray(); };
-}
-function vec2(gl, getter) {
-    return new VertAttr(gl.FLOAT, 2, liftVec(getter));
-}
-exports.vec2 = vec2;
-function vec3(gl, getter) {
-    return new VertAttr(gl.FLOAT, 3, liftVec(getter));
-}
-exports.vec3 = vec3;
-function vec4(gl, getter) {
-    return new VertAttr(gl.FLOAT, 4, liftVec(getter));
-}
-exports.vec4 = vec4;
+// export function short (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
+// {
+//     return new VertAttr (gl.SHORT, 1, lift (getter))
+// }
+// export function ubyte (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
+// {
+//     return new VertAttr (gl.UNSIGNED_BYTE, 1, lift (getter))
+// }
+// export function ushort (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
+// {
+//     return new VertAttr (gl.UNSIGNED_SHORT, 1, lift (getter))
+// }
+// export function float (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
+// {
+//     return new VertAttr (gl.FLOAT, 1, lift (getter))
+// }
+// function liftVec<V extends Vec<V>> (getter: VertAttrGetter<V>): (object) => number[]
+// {
+//     return obj => getter (obj).toArray ()
+// }
+// export function vec2 (gl: WebGLRenderingContext, getter: VertAttrGetter<Vec2>): VertAttr<Vec2>
+// {
+//     return new VertAttr (gl.FLOAT, 2, liftVec (getter))
+// }
+// export function vec3 (gl: WebGLRenderingContext, getter: VertAttrGetter<Vec3>): VertAttr<Vec3>
+// {
+//     return new VertAttr (gl.FLOAT, 3, liftVec (getter))
+// }
+// export function vec4 (gl: WebGLRenderingContext, getter: VertAttrGetter<Vec4>): VertAttr<Vec4>
+// {
+//     return new VertAttr (gl.FLOAT, 4, liftVec (getter))
+// } 
 //# sourceMappingURL=VertexAttribute.js.map
--- src/VertexAttribute.ts
+++ src/VertexAttribute.ts
@@ -1,14 +1,18 @@
 import { Vec, Vec2, Vec3, Vec4 } from "./Vectors";
 
-export type VertAttrGetter<T> = (object) => T
-
-export class VertAttr<T>
+export interface Vertex
 {
+    static attributes: VertAttr[]
+}
+
+export class VertAttr
+{
+    readonly name: string
     readonly type: number
     readonly count: number
     readonly getter: (object) => number[]
 
-    constructor (type: number, count: number, getter: (object) => number[]) 
+    constructor (name: string, type: number, count: number, getter: (object) => number[]) 
     {
         this.type = type
         this.count = count
@@ -38,52 +42,60 @@ export class VertAttr<T>
     }
 }
 
-function lift<T> (fun: (object) => T): (object) => T[]
+class Foo implements Vertex
 {
-    return obj => [ fun (obj) ]
+    attributes: VertAttr[]
+    bar: string
 }
 
-export function byte (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
+export function byte<V extends Vertex, A extends keyof V> (gl: WebGLRenderingContext, vertex: V, name: A) 
 {
-    return new VertAttr (gl.BYTE, 1, lift (getter))
+    vertex.attributes. 
+    return new VertAttr (name, gl.BYTE, 1, v => [ v[name] ])
 }
 
-export function short (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
+function test (gl: WebGLRenderingContext)
 {
-    return new VertAttr (gl.SHORT, 1, lift (getter))
+    let foo = new Foo ()
+    let a = byte (gl, foo, "bar")
 }
 
-export function ubyte (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
-{
-    return new VertAttr (gl.UNSIGNED_BYTE, 1, lift (getter))
-}
+// export function short (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
+// {
+//     return new VertAttr (gl.SHORT, 1, lift (getter))
+// }
 
-export function ushort (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
-{
-    return new VertAttr (gl.UNSIGNED_SHORT, 1, lift (getter))
-}
+// export function ubyte (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
+// {
+//     return new VertAttr (gl.UNSIGNED_BYTE, 1, lift (getter))
+// }
 
-export function float (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
-{
-    return new VertAttr (gl.FLOAT, 1, lift (getter))
-}
+// export function ushort (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
+// {
+//     return new VertAttr (gl.UNSIGNED_SHORT, 1, lift (getter))
+// }
 
-function liftVec<V extends Vec<V>> (getter: VertAttrGetter<V>): (object) => number[]
-{
-    return obj => getter (obj).toArray ()
-}
+// export function float (gl: WebGLRenderingContext, getter: VertAttrGetter<number>): VertAttr<number>
+// {
+//     return new VertAttr (gl.FLOAT, 1, lift (getter))
+// }
 
-export function vec2 (gl: WebGLRenderingContext, getter: VertAttrGetter<Vec2>): VertAttr<Vec2>
-{
-    return new VertAttr (gl.FLOAT, 2, liftVec (getter))
-}
+// function liftVec<V extends Vec<V>> (getter: VertAttrGetter<V>): (object) => number[]
+// {
+//     return obj => getter (obj).toArray ()
+// }
 
-export function vec3 (gl: WebGLRenderingContext, getter: VertAttrGetter<Vec3>): VertAttr<Vec3>
-{
-    return new VertAttr (gl.FLOAT, 3, liftVec (getter))
-}
+// export function vec2 (gl: WebGLRenderingContext, getter: VertAttrGetter<Vec2>): VertAttr<Vec2>
+// {
+//     return new VertAttr (gl.FLOAT, 2, liftVec (getter))
+// }
 
-export function vec4 (gl: WebGLRenderingContext, getter: VertAttrGetter<Vec4>): VertAttr<Vec4>
-{
-    return new VertAttr (gl.FLOAT, 4, liftVec (getter))
-}
+// export function vec3 (gl: WebGLRenderingContext, getter: VertAttrGetter<Vec3>): VertAttr<Vec3>
+// {
+//     return new VertAttr (gl.FLOAT, 3, liftVec (getter))
+// }
+
+// export function vec4 (gl: WebGLRenderingContext, getter: VertAttrGetter<Vec4>): VertAttr<Vec4>
+// {
+//     return new VertAttr (gl.FLOAT, 4, liftVec (getter))
+// }
